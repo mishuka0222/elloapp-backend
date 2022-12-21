ab06aab1bc20bf8ba2310368c06bece343061076
Александр Нагорный <snow1emperor@gmail.com>
2022-12-21 23:59:39 +0200

    [EA-625] fix constructor field in InvokeBizDataRaw;

--- app/bff/bizraw/internal/core/bizraw.bizInvokeBizDataRaw_handler.go
+++ app/bff/bizraw/internal/core/bizraw.bizInvokeBizDataRaw_handler.go
@@ -3,7 +3,9 @@ package core
 import "github.com/teamgram/proto/mtproto"
 
 func (c ConfigurationCore) BizInvokeBizDataRaw(in *mtproto.TLBizInvokeBizDataRaw) (*mtproto.BizDataRaw, error) {
-	c.Logger.Debugf("BizInvokeBizDataRaw data: %+v", in.BizData.Data)
+	c.Logger.Debugf("BizInvokeBizDataRaw data: %+v", string(in.BizData.Data))
 
-	return (&mtproto.BizDataRaw{Data: []byte("other messege")}).To_BizDataRaw().To_BizDataRaw(), nil
+	return (&mtproto.BizDataRaw{
+		Constructor: mtproto.CRC32_bizDataRaw,
+		Data:        []byte("other messege")}).To_BizDataRaw().To_BizDataRaw(), nil
 }

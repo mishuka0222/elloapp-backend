59322f506690d090a2b9b86da8ee0bcbc6ff42a9
Александр Нагорный <nahornyi.merehead@gmail.com>
2023-01-04 11:51:24 +0200

    [EA-668] fix generator.sh script;

--- generator.sh
+++ generator.sh
@@ -4,18 +4,20 @@ GO_CTR_PATH=$PWD/generator/macapp
 
 GOGOPROTO_PATH=$GOPATH/pkg/mod/github.com/gogo/protobuf@v1.3.2/protobuf
 
-WORKDIR=$PWD/app/service/biz/authorization2
+WD_PATH=app/service/biz/authorization2
+WORKDIR=$PWD/$WD_PATH
 SRC_PATH=authorization.proto
 DST_DIR=.
+DST_DIR2=authorization
 
 cd $WORKDIR
 
 $GO_CTR_PATH rpc protoc $SRC_PATH \
   -I=$PWD \
   -I=$GOGOPROTO_PATH \
-  --gogo_dst=authorization \
+  --gogo_dst=$DST_DIR2 \
   --gogo_out="plugins=grpc\,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/api.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/field_mask.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/source_context.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/type.proto=github.com/gogo/protobuf/types"\
   --zrpc_out=$DST_DIR \
   --commands_pkg="gitlab.com/merehead/elloapp/backend/elloapp_tg_backend/pkg2/commands" \
   --mtproto_pkg="gitlab.com/merehead/elloapp/backend/elloapp_tg_backend/mtproto/rpc/metadata" \
-  --verbose
+#  --verbose

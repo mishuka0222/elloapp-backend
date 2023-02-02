b1823086f1c02bd2c0737c225319a05000d15021
makhmudovazeez <azeezmakhmudov@gmail.com>
2023-02-03 00:00:33 +0500

    migration update

--- elloappd/sql/migrate-20230201.sql
+++ elloappd/sql/migrate-20230201.sql
@@ -4,7 +4,7 @@
 -- add columns for `users_ello`
 --
 
-ALTER TABLE `users_ello` ADD COLUMN `type` ENUM('personal', 'business') NOT NULL;
-ALTER TABLE `users_ello` ADD COLUMN `kind` ENUM('public', 'private') NOT NULL;
+ALTER TABLE `users_ello` ADD COLUMN `type` ENUM('personal', 'business') DEFAULT 'personal';
+ALTER TABLE `users_ello` ADD COLUMN `kind` ENUM('public', 'private') DEFAULT 'public';
 ALTER TABLE `users_ello` MODIFY COLUMN `date_of_birth` DATETIME;
 
